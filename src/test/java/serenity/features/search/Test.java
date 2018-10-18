4a8d0d5757ac60344d992f52f933e3d3fe9c4055
tudor <tudormoldovanu@gmail.com>
2018-10-18 13:14:28 +0300

    commit4

---
+++ src/test/java/serenity/features/search/Test.java
@@ -0,0 +1,17 @@
+package serenity.features.search;
+
+public class Test {
+    @org.junit.Test
+    public void test1()
+    {
+        System.out.println("I AM HERE !!!!!!!!!!!");
+        assert(1==1);
+    }
+
+    @org.junit.Test
+    public void test2()
+    {
+        assert(2==3);
+    }
+
+}
