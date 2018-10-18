e5af27c8a5ffd8aea18656321338ae18cb5e43b3
tudor <tudormoldovanu@gmail.com>
2018-10-18 12:54:45 +0300

    commit

---
+++ src/test/java/serenity/Test.java
@@ -0,0 +1,16 @@
+package serenity;
+
+public class Test {
+    @org.junit.Test
+    public void test1()
+    {
+        assert(1==1);
+    }
+
+    @org.junit.Test
+    public void test2()
+    {
+        assert(2==3);
+    }
+
+}
